27249dd870369848400aea491b3e964f30328407
DennisGabriel <dennisgabr745@gmail.com>
2025-03-09 01:37:20 -0300

    add exchanges

---
+++ src/app/styles.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    paddingLeft: 15,
+    paddingTop: 15,
+    display: "flex"
+  },
+
+  input: {
+    borderColor: 'black',
+    borderRadius: 8,
+    borderWidth: 1,
+    width: '80%'
+  }
+})
